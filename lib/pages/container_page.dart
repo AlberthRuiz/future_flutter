01a5179523d828c9d5c61d1ab2f1d8c436d5ce88
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-27 09:36:17 -0500

    header

--- lib/pages/container_page.dart
+++ lib/pages/container_page.dart
@@ -9,8 +9,37 @@ class ContainerPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            //HEADER
+            Container(
+              height: 120,
+              width: double.infinity,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black38.withOpacity(0.3),
+                    offset: Offset(9, 9),
+                    blurRadius: 10,
+                  )
+                ],
+              ),
+              child: Text(
+                "I am a header",
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            //ESTACIONAMIENDO AVIÓN
             Container(
               width: 200,
               height: 200,
