6ced33c0e65d4c72c17758d6ce9f10b105ed0cf5
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-27 13:05:27 -0500

    pasar datos de una pantalla a otra con navigator.pop

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:futuresg7/pages/container_page.dart';
 import 'package:futuresg7/pages/future_page.dart';
+import 'package:futuresg7/pages/screenA.dart';
+import 'package:futuresg7/pages/screenB.dart';
 import 'package:futuresg7/pages/tab_bar_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: FuturePage(),
+      home: ScreenA(),
       debugShowCheckedModeBanner: false,
     ),
   );
