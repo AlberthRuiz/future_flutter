86160e2dfac2b794ad01cb432ceee30730e5a8a6
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-28 12:58:40 -0500

    imageUrl

--- lib/models/trabajo_model.dart
+++ lib/models/trabajo_model.dart
@@ -1,8 +1,10 @@
 class TrabajoModel {
   String posicion;
   String empresa;
+  String imageUrl;
   TrabajoModel({
     required this.posicion,
     required this.empresa,
+    required this.imageUrl,
   });
 }
