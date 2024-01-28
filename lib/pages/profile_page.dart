ab5041316742cb0c7bd59df73eb3ba9242f46d01
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-28 10:11:32 -0500

    ini itnerface

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -3,9 +3,120 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenHeigth = MediaQuery.of(context).size.height;
+    double screnWidth = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
-        body: Stack(),
+        body: Stack(
+          children: [
+            Container(
+              height: screenHeigth,
+              width: screnWidth,
+              color: Colors.grey.shade200,
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 150,
+                width: screnWidth - 40,
+                decoration: BoxDecoration(
+                  color: Color(0xff808FAC),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: EdgeInsets.only(top: 40),
+                height: 200,
+                width: screnWidth - 80,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(25),
+                            child: Image.network(
+                              "https://imgv3.fotor.com/images/gallery/a-man-profile-picture-with-blue-and-green-background-made-by-LinkedIn-Profile-Picture-Maker.jpg",
+                              width: 100,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 24,
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Elliot Thompon",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Desarrollador mòvil",
+                                  style: TextStyle(color: Colors.black38),
+                                ),
+                                SizedBox(
+                                  height: 16,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "Likes",
+                                          style: TextStyle(
+                                            color: Colors.black38,
+                                          ),
+                                        ),
+                                        Text(
+                                          "23",
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text("Folowers"),
+                                        Text("980"),
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text("Rating"),
+                                        Text("8.9"),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
